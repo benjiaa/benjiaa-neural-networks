596c93a519ae77def951fa6a8ea7b96001459b07
benjiaa <48108604+benjiaa@users.noreply.github.com>
2021-09-26 16:28:39 -0400

    Add files via upload

---
+++ project3/optimizer.py
@@ -0,0 +1,160 @@
+'''optimizer.py
+Algorithms to optimize the weights during gradient descent / backprop
+Benji A x Azalea Yunus
+CS343: Neural Networks
+Project 3: Convolutional Neural Networks
+'''
+import numpy as np
+np.random.seed(0)
+
+
+class Optimizer():
+    def __init__(self):
+        self.wts = None
+        self.d_wts = None
+
+    def prepare(self, wts, d_wts):
+        '''Stores weights and their gradient before an update step is performed.
+        '''
+        self.wts = wts
+        self.d_wts = d_wts
+
+    def update_weights(self):
+        pass
+
+    @staticmethod
+    def create_optimizer(name, **kwargs):
+        '''
+        Factory method that takes in a string, and returns a new object of the
+        desired type. Called via Optimizer.create_optimizer().
+        '''
+        if name.lower() == 'sgd':
+            return SGD(**kwargs)
+        elif name.lower() == 'sgd_momentum':
+            return SGD_Momentum(**kwargs)
+        elif name.lower() == 'adam':
+            return Adam(**kwargs)
+        else:
+            raise ValueError('Unknown optimizer name!')
+
+
+class SGD(Optimizer):
+    '''Update weights using Stochastic Gradient Descent (SGD) update rule.
+    '''
+    def __init__(self, lr=0.1):
+        '''
+        Parameters:
+        -----------
+        lr: float > 0. Learning rate.
+        '''
+        self.lr = lr
+
+    def update_weights(self):
+        '''Updates the weights according to SGD and returns a deep COPY of the
+        updated weights for this time step.
+
+        Returns:
+        -----------
+        A COPY of the updated weights for this time step.
+
+        TODO: Write the SGD weight update rule.
+        See notebook for review of equations.
+        '''
+        # update weights with SGD
+        self.wts = self.wts - self.lr * self.d_wts
+        new_wts_sgd = np.copy(self.wts)
+        return new_wts_sgd
+
+class SGD_Momentum(Optimizer):
+    '''Update weights using Stochastic Gradient Descent (SGD) with momentum
+    update rule.
+    '''
+    def __init__(self, lr=0.001, m=0.9):
+        '''
+        Parameters:
+        -----------
+        lr: float > 0. Learning rate.
+        m: float 0 < m < 1. Amount of momentum from gradient on last time step.
+        '''
+        self.lr = lr
+        self.m = m
+        self.velocity = None
+
+    def update_weights(self):
+        '''Updates the weights according to SGD with momentum and returns a
+        deep COPY of the updated weights for this time step.
+
+        Returns:
+        -----------
+        A COPY of the updated weights for this time step.
+
+        TODO: Write the SGD with momentum weight update rule.
+        See notebook for review of equations.
+        '''
+        # check for first time step to initialize v
+        if self.velocity is None:
+            self.velocity = np.zeros(shape=self.wts.shape)
+        else:
+            pass
+        # update weights with SGDM   
+        self.velocity = self.m * self.velocity - self.lr * self.d_wts
+        self.wts = self.wts + self.velocity
+        new_wts_sgdm = np.copy(self.wts)
+        return new_wts_sgdm
+
+class Adam(Optimizer):
+    '''Update weights using the Adam update rule.
+    '''
+    def __init__(self, lr=0.001, beta1=0.9, beta2=0.999, eps=1e-8, t=0):
+        '''
+        Parameters:
+        -----------
+        lr: float > 0. Learning rate.
+        beta1: float 0 < m < 1. Amount of momentum from gradient on last time step.
+        beta2: float 0 < m < 1. Amount of momentum from gradient on last time step.
+        eps: float. Small number to prevent division by 0.
+        t: int. Records the current time step: 0, 1, 2, ....
+        '''
+        self.lr = lr
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.eps = eps
+        self.t = t
+
+        self.m = None
+        self.v = None
+
+    def update_weights(self):
+        '''Updates the weights according to Adam and returns a
+        deep COPY of the updated weights for this time step.
+
+        Returns:
+        -----------
+        A COPY of the updated weights for this time step.
+
+        TODO: Write the Adam update rule
+        See notebook for review of equations.
+
+        Hints:
+        -----------
+        - Remember to initialize m and v.
+        - Remember that t should = 1 on the 1st wt update.
+        - Remember to update/save the new values of m, v between updates.
+        '''
+        # check for first time step to initialize m and v
+        if self.m is None:
+            self.m = np.zeros(shape=self.wts.shape)
+            self.v = np.zeros(shape=self.wts.shape)
+            self.t = 1
+        else:
+            pass
+        # update weights with ADAM
+        # t is 1 on first update
+        self.m = self.beta1 * self.m + (1 - self.beta1) * self.d_wts
+        self.v = self.beta2 * self.v + (1 - self.beta2) * self.d_wts ** 2
+        n = self.m / (1 - self.beta1 ** self.t)
+        u = self.v / (1 - self.beta2 ** self.t)
+        self.wts = self.wts - (self.lr * n) / (np.sqrt(u) + self.eps)
+        self.t += 1 
+        new_wts_adam = np.copy(self.wts)
+        return new_wts_adam
